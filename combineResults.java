2b3b9f43751bdbac2db08306535547cbfe22c9a8
Priyal Sharma <157181217+preelshh@users.noreply.github.com>
2025-07-03 12:32:53 +0200

    Update combineResults.java
    Add targeted debug logging to trace pellet data processing
    
    - Log each pellet file being processed (file path)
    - Output the number of rows detected in the ImageJ ResultsTable
    - Output the column headings present in the ResultsTable
    - Confirm the size of the array added to biggestArrayPellets
    
    These debug statements help identify whether no files were matched, the ResultsTable was empty, or expected columns were missing, which leads to the NoSuchElementException when Collections.max() is called on an empty list.

--- combineResults.java
+++ combineResults.java
@@ -150,6 +150,15 @@
 /* 152 */       for (i = 0; i < listOfResultFiles.length; i++) {
 /* 154 */         String resultFilePath = listOfResultFiles[i].getAbsolutePath();
 /* 156 */         if (listOfResultFiles[i].isFile() && resultFilePath.endsWith(this.resultFileSuffixPellets)) {
+                    System.out.println("---- Processing Pellet File: " + resultFilePath);
+
+                    ResultsTable resultTablePellets = ResultsTable.getResultsTable();
+                    int nResults = resultTablePellets.size();
+                    System.out.println("Number of rows in ResultsTable: " + nResults);
+
+                    String[] headings = resultTablePellets.getHeadings();
+                    System.out.println("Columns in ResultsTable: " + java.util.Arrays.toString(headings));
+
 /* 158 */           counterPelletCvs++;
 /* 160 */           IJ.open(resultFilePath);
 /* 161 */           ResultsTable resultTablePellets = ResultsTable.getResultsTable();
@@ -169,6 +178,8 @@
 /*     */           } 
 /* 182 */           int bigArrayPellets = this.areaPellets.size();
 /* 183 */           this.biggestArrayPellets.add(Integer.valueOf(bigArrayPellets));
+                    System.out.println("bigArrayPellets added: " + bigArrayPellets);
+
 /* 185 */           this.totAreaPellets.add(Double.toString(totalAreaPellets));
 /* 187 */           this.allAreaPellets.add(this.areaPellets);
 /* 188 */           this.allSolidityPellets.add(this.solidityPellets);
